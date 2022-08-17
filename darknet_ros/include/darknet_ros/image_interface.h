537db5689c8a298316f8ddf122beb81dfb08d96b
muneeb <munn33b@gmail.com>
2022-08-17 15:07:42 -0400

    Added Files for Custom Trained Model

--- darknet_ros/include/darknet_ros/image_interface.h
+++ darknet_ros/include/darknet_ros/image_interface.h
@@ -10,10 +10,10 @@
 #define IMAGE_INTERFACE_H
 
 #include "image.h"
-#include "opencv2/opencv.hpp"
+#include <opencv2/core.hpp>
 
 static float get_pixel(image m, int x, int y, int c);
-image** load_alphabet_with_file(char* datafile);
-void generate_image(image p, cv::Mat& disp);
+image **load_alphabet_with_file(char *datafile);
+void generate_image(image p, cv::Mat disp);
 
 #endif
